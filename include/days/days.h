349a4f7e4d007d44c4cf9c1b0a4cbf4894eef062
ondesly <dmitriytorkhov@gmail.com>
2021-02-10 13:36:48 +0300

    add cmake and source files

---
+++ include/days/days.h
@@ -0,0 +1,83 @@
+//
+//  days.h
+//  days
+//
+//  Created by Dmitrii Torkhov <dmitriitorkhov@gmail.com> on 09.02.2021.
+//  Copyright © 2021 Dmitrii Torkhov. All rights reserved.
+//
+
+#pragma once
+
+#include <ctime>
+#include <iterator>
+
+namespace oo {
+
+    class day {
+    public:
+
+        explicit day(time_t t);
+
+    public:
+
+        day &operator++();
+
+        bool operator==(const day &other) const;
+
+        time_t get_begin() const;
+
+        time_t get_end() const;
+
+    private:
+
+        tm m_tm;
+
+    };
+
+}
+
+namespace oo {
+
+    class days {
+    public:
+
+        class iterator : public std::iterator<std::forward_iterator_tag, oo::day, size_t,
+                oo::day *, const oo::day &> {
+        public:
+
+            explicit iterator(const oo::day &day);
+
+        public:
+
+            reference operator*() const;
+
+            iterator &operator++();
+
+            bool operator==(const iterator &other) const;
+
+            bool operator!=(const iterator &other) const;
+
+        private:
+
+            oo::day m_day;
+
+        };
+
+    public:
+
+        days(time_t begin, time_t end);
+
+    public:
+
+        iterator begin() const;
+
+        iterator end() const;
+
+    private:
+
+        time_t m_begin;
+        time_t m_end;
+
+    };
+
+}
---
+++ src/days/days.cpp
@@ -0,0 +1,77 @@
+//
+//  days.cpp
+//  days
+//
+//  Created by Dmitrii Torkhov <dmitriitorkhov@gmail.com> on 09.02.2021.
+//  Copyright © 2021 Dmitrii Torkhov. All rights reserved.
+//
+
+#include "days/days.h"
+
+// -- day --
+
+oo::day::day(time_t t) {
+    m_tm = *std::localtime(&t);
+    m_tm.tm_hour = m_tm.tm_min = m_tm.tm_sec = 0;
+}
+
+oo::day &oo::day::operator++() {
+    ++m_tm.tm_mday;
+
+    return *this;
+}
+
+bool oo::day::operator==(const oo::day &other) const {
+    auto tm1 = m_tm;
+    auto tm2 = other.m_tm;
+
+    return std::mktime(&tm1) == std::mktime(&tm2);
+}
+
+time_t oo::day::get_begin() const {
+    auto tm = m_tm;
+    return std::mktime(&tm);
+}
+
+time_t oo::day::get_end() const {
+    auto tm = m_tm;
+    tm.tm_mday += 1;
+    return std::mktime(&tm) - 1;
+}
+
+// -- days --
+
+oo::days::days(time_t begin, time_t end) : m_begin(begin), m_end(end) {}
+
+oo::days::iterator oo::days::begin() const {
+    return iterator{day{m_begin}};
+}
+
+oo::days::iterator oo::days::end() const {
+    auto tm = std::localtime(&m_end);
+    tm->tm_mday += 1;
+
+    return iterator{day{std::mktime(tm)}};
+}
+
+// -- iterator --
+
+oo::days::iterator::iterator(const oo::day &day) : m_day(day) {}
+
+oo::days::iterator::reference oo::days::iterator::operator*() const {
+    return m_day;
+}
+
+oo::days::iterator &oo::days::iterator::operator++() {
+    ++m_day;
+
+    return *this;
+}
+
+bool oo::days::iterator::operator==(const oo::days::iterator &other) const {
+    return m_day == other.m_day;
+}
+
+bool oo::days::iterator::operator!=(const oo::days::iterator &other) const {
+    return !(operator==(other));
+}
